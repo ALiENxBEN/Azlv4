f9718c767ecc3cf0a6d78aa91b1024fb6d9b0eef
Anuragben10 <112600905+Anuragben10@users.noreply.github.com>
2023-06-26 18:47:10 +0530

    __main__.py

---
+++ bot/__main__.py
@@ -0,0 +1,139 @@
+    end_time = int(round(time() * 1000))
+    await editMessage(reply, BotTheme('PING_VALUE', value=(end_time - start_time)))
+
+
+async def log(client, message):
+    await sendFile(message, 'log.txt')
+
+
+async def search_images():
+    if not config_dict['IMG_SEARCH']:
+        return
+    try:
+        query_list = config_dict['IMG_SEARCH']
+        total_pages = config_dict['IMG_PAGE']
+        base_url = "https://www.wallpaperflare.com/search"
+
+        for query in query_list:
+            query = query.strip().replace(" ", "+")
+            for page in range(1, total_pages + 1):
+                url = f"{base_url}?wallpaper={query}&width=1280&height=720&page={page}"
+                r = rget(url)
+                soup = BeautifulSoup(r.text, "html.parser")
+                images = soup.select('img[data-src^="https://c4.wallpaperflare.com/wallpaper"]')
+                for img in images:
+                    img_url = img['data-src']
+                    if img_url not in config_dict['IMAGES']:
+                        config_dict['IMAGES'].append(img_url)
+            if DATABASE_URL:
+                await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+    except Exception as e:
+        LOGGER.error(f"An error occurred: {e}")
+
+
+help_string = f'''
+NOTE: Try each command without any argument to see more detalis.
+/{BotCommands.MirrorCommand[0]} or /{BotCommands.MirrorCommand[1]}: Start mirroring to Google Drive.
+/{BotCommands.QbMirrorCommand[0]} or /{BotCommands.QbMirrorCommand[1]}: Start Mirroring to Google Drive using qBittorrent.
+/{BotCommands.YtdlCommand[0]} or /{BotCommands.YtdlCommand[1]}: Mirror yt-dlp supported link.
+/{BotCommands.LeechCommand[0]} or /{BotCommands.LeechCommand[1]}: Start leeching to Telegram.
+/{BotCommands.QbLeechCommand[0]} or /{BotCommands.QbLeechCommand[1]}: Start leeching using qBittorrent.
+/{BotCommands.YtdlLeechCommand[0]} or /{BotCommands.YtdlLeechCommand[1]}: Leech yt-dlp supported link.
+/{BotCommands.CloneCommand} [drive_url]: Copy file/folder to Google Drive.
+/{BotCommands.CountCommand} [drive_url]: Count file/folder of Google Drive.
+/{BotCommands.DeleteCommand} [drive_url]: Delete file/folder from Google Drive (Only Owner & Sudo).
+/{BotCommands.UserSetCommand} [query]: Users settings.
+/{BotCommands.BotSetCommand} [query]: Bot settings.
+/{BotCommands.BtSelectCommand}: Select files from torrents by gid or reply.
+/{BotCommands.CancelMirror}: Cancel task by gid or reply.
+/{BotCommands.CancelAllCommand} [query]: Cancel all [status] tasks.
+/{BotCommands.ListCommand} [query]: Search in Google Drive(s).
+/{BotCommands.SearchCommand} [query]: Search for torrents with API.
+/{BotCommands.StatusCommand}: Shows a status of all the downloads.
+/{BotCommands.StatsCommand}: Show stats of the machine where the bot is hosted in.
+/{BotCommands.PingCommand}: Check how long it takes to Ping the Bot (Only Owner & Sudo).
+/{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Only Owner & Sudo).
+/{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Only Owner & Sudo).
+/{BotCommands.UsersCommand}: show users settings (Only Owner & Sudo).
+/{BotCommands.AddSudoCommand}: Add sudo user (Only Owner).
+/{BotCommands.RmSudoCommand}: Remove sudo users (Only Owner).
+/{BotCommands.RestartCommand}: Restart and update the bot (Only Owner & Sudo).
+/{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports (Only Owner & Sudo).
+/{BotCommands.ShellCommand}: Run shell commands (Only Owner).
+/{BotCommands.EvalCommand}: Run Python Code Line | Lines (Only Owner).
+/{BotCommands.ExecCommand}: Run Commands In Exec (Only Owner).
+/{BotCommands.ClearLocalsCommand}: Clear {BotCommands.EvalCommand} or {BotCommands.ExecCommand} locals (Only Owner).
+/{BotCommands.RssCommand}: RSS Menu.
+'''
+
+
+async def bot_help(client, message):
+    await sendMessage(message, help_string)
+
+
+async def restart_notification():
+    now=datetime.now(timezone('Asia/Dhaka'))
+    if await aiopath.isfile(".restartmsg"):
+        with open(".restartmsg") as f:
+            chat_id, msg_id = map(int, f)
+    else:
+        chat_id, msg_id = 0, 0
+
+    async def send_incompelete_task_message(cid, msg):
+        try:
+            if msg.startswith(BotTheme('RESTART_SUCCESS')):
+                await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text=msg)
+                await aioremove(".restartmsg")
+            else:
+                await bot.send_message(chat_id=cid, text=msg, disable_web_page_preview=True,
+                                       disable_notification=True)
+        except Exception as e:
+            LOGGER.error(e)
+
+    if INCOMPLETE_TASK_NOTIFIER and DATABASE_URL:
+        if notifier_dict := await DbManger().get_incomplete_tasks():
+            for cid, data in notifier_dict.items():
+                msg = BotTheme('RESTART_SUCCESS', time=now.strftime('%I:%M:%S %p'), date=now.strftime('%d/%m/%y'))if cid == chat_id else BotTheme('RESTARTED')
+                for tag, links in data.items():
+                    msg += f"\n\n➲ {tag}: "
+                    for index, link in enumerate(links, start=1):
+                        msg += f" <a href='{link}'>{index}</a> |"
+                        if len(msg.encode()) > 4000:
+                            await send_incompelete_task_message(cid, msg)
+                            msg = ''
+                if msg:
+                    await send_incompelete_task_message(cid, msg)
+
+    if await aiopath.isfile(".restartmsg"):
+        try:
+            await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text=BotTheme('RESTART_SUCCESS', time=now.strftime('%I:%M:%S %p'), date=now.strftime('%d/%m/%y')))
+        except:
+            pass
+        await aioremove(".restartmsg")
+
+
+async def main():
+    await gather(start_cleanup(), torrent_search.initiate_search_tools(), restart_notification(), search_images(), set_commands(bot))
+    await sync_to_async(start_aria2_listener, wait=False)
+    
+    bot.add_handler(MessageHandler(
+        start, filters=command(BotCommands.StartCommand) & private))
+    bot.add_handler(CallbackQueryHandler(
+        token_callback, filters=regex(r'^pass')))
+    bot.add_handler(MessageHandler(
+        login, filters=command(BotCommands.LoginCommand) & private))
+    bot.add_handler(MessageHandler(log, filters=command(
+        BotCommands.LogCommand) & CustomFilters.sudo))
+    bot.add_handler(MessageHandler(restart, filters=command(
+        BotCommands.RestartCommand) & CustomFilters.sudo))
+    bot.add_handler(MessageHandler(ping, filters=command(
+        BotCommands.PingCommand) & CustomFilters.authorized))
+    bot.add_handler(MessageHandler(bot_help, filters=command(
+        BotCommands.HelpCommand) & CustomFilters.authorized))
+    bot.add_handler(MessageHandler(stats, filters=command(
+        BotCommands.StatsCommand) & CustomFilters.authorized))
+    LOGGER.info("Bot Started!")
+    signal(SIGINT, exit_clean_up)
+
+bot.loop.run_until_complete(main())
+bot.loop.run_forever()
